a05c3a2e0ec5f157f2b589b1ad22af84c584bf63
John Kouris <jdkouris@gmail.com>
2020-04-22 13:02:53 -0700

    Configured the CTSecondaryTitleLabel

--- CovidTracker/CovidTracker/Custom Views/Labels/CTSecondaryTitleLabel.swift
+++ CovidTracker/CovidTracker/Custom Views/Labels/CTSecondaryTitleLabel.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class CTSecondaryTitleLabel: UILabel {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(fontSize: CGFloat) {
+        self.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 
 }
