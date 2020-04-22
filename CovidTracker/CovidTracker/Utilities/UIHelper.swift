d08cd18e65c97c8fe5028f314f2a1ecdcf3468fa
John Kouris <jdkouris@gmail.com>
2020-04-22 10:14:29 -0700

    Added the UIHelper file

---
+++ CovidTracker/CovidTracker/Utilities/UIHelper.swift
@@ -0,0 +1,15 @@
+//
+//  UIHelper.swift
+//  CovidTracker
+//
+//  Created by John Kouris on 4/22/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+enum UIHelper {
+    
+    
+    
+}
