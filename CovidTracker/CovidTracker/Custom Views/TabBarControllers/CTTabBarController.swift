997da2ef4a3923b3ff5f5fa2de3b4d1d85905e63
John Kouris <jdkouris@gmail.com>
2020-04-22 08:46:36 -0700

    Added the StatesVC and SnapshotVC files

--- CovidTracker/CovidTracker/Custom Views/TabBarControllers/CTTabBarController.swift
+++ CovidTracker/CovidTracker/Custom Views/TabBarControllers/CTTabBarController.swift
@@ -13,7 +13,7 @@ class CTTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
 }
