df87745b0ae39659f03bf4e0aa98f6cdba646a6d
John Kouris <jdkouris@gmail.com>
2020-04-22 10:45:48 -0700

    Configured the StateCell. Still need to load the right state icons

--- CovidTracker/CovidTracker/Custom Views/Cells/StateCell.swift
+++ CovidTracker/CovidTracker/Custom Views/Cells/StateCell.swift
@@ -9,5 +9,39 @@
 import UIKit
 
 class StateCell: UICollectionViewCell {
+    static let reuseID = "StateCell"
+    let stateIconView = UIImageView(frame: .zero)
+    let stateNameLabel = CTTitleLabel(textAlignment: .center, fontSize: 16)
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(state: State) {
+        stateNameLabel.text = state.state
+        stateIconView.image = UIImage(named: "california")
+    }
+    
+    private func configure() {
+        addSubviews(stateIconView, stateNameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            stateIconView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            stateIconView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            stateIconView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            stateIconView.heightAnchor.constraint(equalTo: stateIconView.widthAnchor),
+            
+            stateNameLabel.topAnchor.constraint(equalTo: stateIconView.bottomAnchor, constant: 12),
+            stateNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            stateNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            stateNameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
 }
