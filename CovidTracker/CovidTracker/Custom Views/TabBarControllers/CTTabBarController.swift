12223bcf68ed1850ebfda0fd643e9281d0c18d56
John Kouris <jdkouris@gmail.com>
2020-04-22 08:43:22 -0700

    Added the CTTabBarController file

---
+++ CovidTracker/CovidTracker/Custom Views/TabBarControllers/CTTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  CTTabBarController.swift
+//  CovidTracker
+//
+//  Created by John Kouris on 4/22/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class CTTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
