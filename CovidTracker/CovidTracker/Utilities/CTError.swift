963a948dc8882e637dddb478941999158a3feff0
John Kouris <jdkouris@gmail.com>
2020-04-22 09:56:53 -0700

    Configured the CTError enum

--- CovidTracker/CovidTracker/Utilities/CTError.swift
+++ CovidTracker/CovidTracker/Utilities/CTError.swift
@@ -7,3 +7,9 @@
 //
 
 import Foundation
+
+enum CTError: String, Error {
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
