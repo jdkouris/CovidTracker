11b23ba6d95c1d7cdd8a69e00e8e1f5cc06beb6d
John Kouris <jdkouris@gmail.com>
2020-04-22 10:32:23 -0700

    Added the CTLabel to the project

---
+++ CovidTracker/CovidTracker/Custom Views/Labels/CTTitleLabel.swift
@@ -0,0 +1,21 @@
+//
+//  CTTitleLabel.swift
+//  CovidTracker
+//
+//  Created by John Kouris on 4/22/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class CTTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
